cf6e7447d35d5d854d2a17c19f1d022d9b286c3f
dynaudio <dynaudioputra25@gmail.com>
2022-12-01 14:46:43 +0800

    Halaman Login

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import { loggeduser } from '../redux/action/loginmasukAction'
+import axios from 'axios'
+
+
+function LoginForm() {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const url = 'https://be-production-85d3.up.railway.app/user/login'
+
+
+    function handleTes(e) {
+        e.preventDefault()
+        axios.post(url, {
+            email: email,
+            password: password
+        }
+        
+        )
+        .then((res) => {
+            alert(res.data.message)
+            localStorage.setItem("token", res.data.token)
+            localStorage.setItem("id_user", res.data.id_user)
+            dispatch(loggeduser(true))
+            navigate('/')
+        })
+    }
+ 
+
+
+    return (
+        <section>
+        
+        <div id="login-wrap">
+            <div id="login-box">
+                <form id="btn" onSubmit={handleTes}>
+                    <h5>Sudah Mempunyai Akun?</h5>
+                    <h5>Silahkan Login</h5>
+                    <div className="form-box">
+                        <p>Email<span> *</span></p>
+                        <div className="input-wrapper" name="for-nama-belakang">
+                            <input type="email" id="email" className="form-input" name="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
+                        </div>
+                        
+                    </div>
+                    <div className="form-box">
+                        <p>Password<span> *</span></p>
+                        <div className="input-wrapper" name="for-nama-belakang">
+                            <input type="password" id="password" className="form-input" name="password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
+                        </div>
+                        
+                    </div>
+                    
+                    <div className="one-line">
+                        <div id="wrap-ingat">
+                            <input type="checkbox" id="ingat" className="wr" name="ingat"/>
+                            <p>Ingat Saya</p>
+                        </div>
+                        
+                        <p><a href="" className="press">Lupa Password?</a></p>
+                    </div>
+                    <div className="log">
+                        <button type="submit" className="log-button">Login</button>
+                    </div>
+                    <h6>Belum Mempunyai Akun? <span><Link to='/register' className="press">Register Disini</Link></span></h6>
+                </form>
+                
+            </div>
+        </div>
+    </section>
+    )
+}
+
+export default LoginForm
