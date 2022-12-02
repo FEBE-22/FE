f753c83e6e6f12e076685baa6eb68f2d9d566a35
dynaudio <dynaudioputra25@gmail.com>
2022-12-02 17:03:42 +0800

    Halaman detail dan pembayaran

---
+++ src/pages/Dokterpembayaran.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Nav from "../components/Nav";
+import Bayar from "../components/Bayar"
+import './Dokterpembayaran.css'
+
+function Dokterpembayaran(){
+    return (
+        <>
+            <Nav/>
+            <Bayar/>
+        </>
+        
+    )
+}
+
+
+export default Dokterpembayaran
